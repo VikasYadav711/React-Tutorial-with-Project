ea3405aa132fa75e03a96bfcf328b5408cce0ba2
VIKAS YADAV <vikas.savy117@gmail.com>
2024-04-13 02:44:41 +0530

    version2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-//first component
+import './index.css';
 
+//first component
+/*
 function Greeting(){
     return (
         <div>
@@ -17,8 +19,47 @@ const Message = () => <h3> Message</h3>;
 const Person =() => {
     return <p> This is John Doe</p>;
 };
+*/
+
+const BookList= ()=>{
+    return (
+        <section className='booklist'>
+            <Book/>
+            <Book/>
+            <Book/>
+        </section>
+    );
+};
+
+const Book=()=>{
+    return (
+        <article className='book'>
+            <Image/>
+            <Title/>
+            <Author/>
+        </article>
+    );
+};
+
+const Image=()=>{
+    return(
+        <img src='./images/1.jpg' 
+        alt='The Women: A Novel' />
+    );
+};
+
+const Title=()=>{
+    return(
+        <h2>The Women</h2>
+    );
+};
+
+const Author=()=>{
+    return <h4>Kristin Hannah</h4>;
+};
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting/>);
+root.render(<BookList/>);
