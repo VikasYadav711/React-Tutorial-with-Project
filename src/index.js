080b37844bcd2b09b3af52f213ae8a627af05d8c
VIKAS YADAV <vikas.savy117@gmail.com>
2024-04-13 02:51:59 +0530

    version3inlineStyle

--- src/index.js
+++ src/index.js
@@ -55,7 +55,13 @@ const Title=()=>{
 };
 
 const Author=()=>{
-    return <h4>Kristin Hannah</h4>;
+    const inlineHeadingStyles={
+        color: '#617d98',
+        fontSize: '0.75rem',
+        marginTop: '0.5rem',
+    };
+
+    return <h4 style={inlineHeadingStyles}>Kristin Hannah</h4>;
 };
 
 
